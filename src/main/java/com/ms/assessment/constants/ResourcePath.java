3dc923784cfaebe2df527143349279bdee9b4661
garlok <garlokloh@gmail.com>
2024-07-21 02:29:20 +0800

    Task 4 commit

--- src/main/java/com/ms/assessment/constants/ResourcePath.java
+++ src/main/java/com/ms/assessment/constants/ResourcePath.java
@@ -3,6 +3,6 @@ package com.ms.assessment.constants;
 public class ResourcePath {
     public static final String BINANCE_API_URL = "https://api.binance.com/api/v3/ticker/bookTicker";
     public static final String HUOBI_API_URL = "https://api.huobi.pro/market/tickers";
-    public static final String WALLETS = "/wallets";
+    public static final String WALLETS = "/api/wallets";
     public static final String GET_BALANCE_API_URL = "/balance/{owner}";
 }
